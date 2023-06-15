fecb1fb035b8fd4f4cc313fdea665b40202990b3
yosef <93218993+yossi2021@users.noreply.github.com>
2023-06-15 18:12:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// const myFather = new Person("John", "Doe", 50, "blue");
+//   const myMother = new Person("Sally", "Rally", 48, "green");
+
+// function Person(first, last, age, eyecolor) {
+//     this.firstName = first;
+//     this.lastName = last;
+//     this.age = age;
+//     this.eyeColor = eyecolor;
+//   }
+//   if (= "Doe") {
+//     prompt('good')
+//   } else {
+//     alert('not good')
+//   };
+//   function searchbook(){
+//   if (value = 'ספרי נשים') {
+//     window.open( href="./w.books.html");
+//   } else {
+//     window.open( href="./contactus.html");
+//   }
+// }
